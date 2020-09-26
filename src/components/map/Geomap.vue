a20b51ae46b3885bffe87a69bb771db6a146e14c
JayVnl <jorrit.v@outlook.com>
2020-09-26 17:07:23 +0200

    improved geomap styling and created separate component

---
+++ src/components/map/Geomap.vue
@@ -0,0 +1,90 @@
+<template>
+  <div>
+    <Sort @changeActive="changeMap" :marginY="10" :marginX="20"></Sort>
+    <div class="map-wrap">
+      <iframe
+        class="localfocusvisual"
+        frameborder="0"
+        style="width:100%;height:550px;overflow:hidden"
+        :src="src"
+      ></iframe>
+    </div>
+  </div>
+</template>
+
+<script>
+import Sort from "@/components/sort/Sort.vue";
+
+export default {
+  name: "Map",
+  data() {
+    return {
+      theme: localStorage.getItem("themeColor"),
+      src: "https://localfocuswidgets.net/5f6dd9b6f2be6"
+    };
+  },
+  methods: {
+    switchType(id, rep, hos, dec) {
+      switch (id) {
+        case "rep":
+          this.src = rep;
+          break;
+        case "hos":
+          this.src = hos;
+          break;
+        case "dec":
+          this.src = dec;
+          break;
+      }
+    },
+    changeMap(clicked) {
+      if (this.theme == "dark-mode") {
+        this.switchType(
+          clicked.id,
+          "https://localfocuswidgets.net/5f6dd9b6f2be6",
+          "https://localfocuswidgets.net/5f6dd9dc51de6",
+          "https://localfocuswidgets.net/5f6dda059c5f3"
+        );
+      } else {
+        this.switchType(
+          clicked.id,
+          "https://localfocuswidgets.net/5f6dcfee36092",
+          "https://localfocuswidgets.net/5f6dbe6b4a836",
+          "https://localfocuswidgets.net/5f6dbe670aadb"
+        );
+      }
+    }
+  },
+  mounted() {
+    this.theme == "dark-mode"
+      ? (this.src = "https://localfocuswidgets.net/5f6dd9b6f2be6")
+      : (this.src = "https://localfocuswidgets.net/5f6dcfee36092");
+  },
+  components: {
+    Sort
+  }
+};
+</script>
+
+<style scoped>
+.map-wrap {
+  width: auto;
+  height: 459px;
+  margin: 0 20px;
+  box-shadow: 0px 0px 6px 2px rgba(0, 0, 0, 0.1);
+  border-radius: 12px;
+  transform: translateZ(0px);
+  overflow: hidden;
+}
+
+.localfocusvisual {
+  margin-top: -50px;
+  transform: scale(1.05);
+}
+
+@media only screen and (max-width: 328px) {
+  .map-wrap {
+    height: 440px;
+  }
+}
+</style>
--- src/views/Map.vue
+++ src/views/Map.vue
@@ -2,15 +2,7 @@
   <div>
     <Topbar title="MAP" :map="false" :settings="false"></Topbar>
     <Section1 :top="90">
-      <Sort @changeActive="changeMap" :marginY="10" :marginX="20"></Sort>
-      <div class="map-wrap">
-        <iframe
-          class="localfocusvisual"
-          frameborder="0"
-          style="width:100%;height:550px;overflow:hidden"
-          :src="src"
-        ></iframe>
-      </div>
+      <Geomap></Geomap>
     </Section1>
   </div>
 </template>
@@ -18,66 +10,14 @@
 <script>
 import Topbar from "@/components/nav/Topbar.vue";
 import Section1 from "@/components/sections/Section1.vue";
-import Sort from "@/components/sort/Sort.vue";
+import Geomap from "@/components/map/Geomap.vue";
 
 export default {
   name: "Map",
-  data() {
-    return {
-      theme: localStorage.getItem("themeColor"),
-      src: "https://localfocuswidgets.net/5f6dd9b6f2be6"
-    };
-  },
-  methods: {
-    changeMap(clicked) {
-      if (this.theme == "dark-mode") {
-        switch (clicked.id) {
-          case "rep":
-            this.src = "https://localfocuswidgets.net/5f6dd9b6f2be6";
-            break;
-          case "hos":
-            this.src = "https://localfocuswidgets.net/5f6dd9dc51de6";
-            break;
-          case "dec":
-            this.src = "https://localfocuswidgets.net/5f6dda059c5f3";
-            break;
-        }
-      } else {
-        switch (clicked.id) {
-          case "rep":
-            this.src = "https://localfocuswidgets.net/5f6dcfee36092";
-            break;
-          case "hos":
-            this.src = "https://localfocuswidgets.net/5f6dbe6b4a836";
-            break;
-          case "dec":
-            this.src = "https://localfocuswidgets.net/5f6dbe670aadb";
-            break;
-        }
-      }
-    }
-  },
-  mounted() {
-    this.theme == "dark-mode"
-      ? (this.src = "https://localfocuswidgets.net/5f6dd9b6f2be6")
-      : (this.src = "https://localfocuswidgets.net/5f6dcfee36092");
-  },
   components: {
     Topbar,
     Section1,
-    Sort
+    Geomap
   }
 };
 </script>
-
-<style scoped>
-.map-wrap {
-  width: 100%;
-  height: 450px;
-  overflow: hidden;
-}
-
-.localfocusvisual {
-  margin-top: -50px;
-}
-</style>
